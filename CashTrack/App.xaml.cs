f74e132632fd8e67e82a9b91b146d56f9b6e8e04
hp <hp@LAPTOP-38HDMDQQ>
2024-12-17 13:24:40 +0545

    Add project files.

---
+++ CashTrack/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CashTrack
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
